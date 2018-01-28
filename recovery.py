5fcc83c7e84622a2c789087dd0f3989f80896cb6
scwalll <deselys.pascal@gmail.com>
2018-01-28 20:39:22 +0100

    modify recovery, add check table is exist

--- recovery.py
+++ recovery.py
@@ -1,18 +1,16 @@
 # coding: utf8
-from multiprocessing import Pool
+import sys
 
-import multiprocessing
 import requests
-import os
-from packages.databases.query_models import CategoriesQuery, ProductsQuery, ConnectionQuery
-from packages.databases.models import Categories, Products, Link_category_product
+import time
+
 from packages.databases.add_information_function import add_information_connection
 from packages.databases.databases import Database
+from packages.databases.models import Categories, Products
+from packages.databases.query_models import CategoriesQuery, ConnectionQuery
+from packages.functions import clr, percentage_calculation
 
-# from packages.products import Products
-# from packages.link_categories_products import Link_category_product
-from packages.functions import cls, percentage_calculation
-
+clr()
 print("Bienvenue dans la récupération des données du site openfoodfact\n"
       "La récupération des catégories et des produits peut prendre plusieurs heures\n"
       "Veuillez ne pas éteindre votre ordinateur pendant la récupération\n"
@@ -20,47 +18,70 @@ print("Bienvenue dans la récupération des données du site openfoodfact\n"
       )
 command = input("> ")
 if command.lower() == "o":
-    cls()
+    clr()
     print("Connexion à la base de données")
     connection = Database()
     if connection.result_connection["error"] is not False:
         add_information_connection()
     print("Connexion à la base de données réussie")
+    connection.connect_databases()
+    ConnectionQuery.set_connection(connection)
+    if False in connection.if_exist_table("categories", "link_category_product", "products", "save_products"):
+        print("Vous n'avez pas lancé le script de création de la base de données\n ou une table a été supprimée\n")
+        print("Suppression des tables")
+        connection.drop_table()
+        print("Création des tables")
+        connection.create_table()
+    already_saved_categories = CategoriesQuery.get_categories_count()
 
-    # def url_request(url, number):
-    #     """
-    #     :param url: url for modification for the pages
-    #     :type url: str
-    #     :param number: number for the page
-    #     :type number: int
-    #     :return: return json file
-    #     """
-    #     url_number = str(url) + "/" + str(number) + ".json"
-    #     r = requests.get(url_number)
-    #     return r.json()
+    if already_saved_categories > 1:
+        already_saved_boucle = True
+        while already_saved_boucle is True:
+            print("vous semblez avoir déjà récupéré les données sur openfoodfact\n "
+                  "il y a actuellement " + str(already_saved_categories) + " Categories dans votre base de données")
+            print(
+                "Si l'application à planté ou qu'il y a eu une erreur "
+                "et que vous voulez réexecuter la récupération, "
+                "veuillez confirmer par 'OUI' Pour recommencer depuis le début ou 'NON' (en majuscule) pour annuler\n"
+                "'RESUME' pour reprendre la ou le programme c'est arrêté")
+
+            command = input("> ")
+            if command == 'OUI':
+                connection.connect.expunge_all()
+                connection.drop_table()
+                connection.create_table()
+            if command == 'NON':
+                sys.exit("Annulation")
+
     count = 0
     total_count = 0
-    print("Début de la récupération des catégories\n"
-          "Connexion au site openfoodfact\n"
-          )
+    print("Connexion au site openfoodfact\n")
     categories_json = requests.get("https://fr.openfoodfacts.org/categories.json")
     print("Connexion réussie au site openfoodfact")
     categories_dic = categories_json.json()
-    connection.connect_databases()
-    ConnectionQuery.set_connection(connection)
     total_count = categories_dic['count']
     for categories in categories_dic['tags']:
         if int(categories['products']) > 10 or str(categories['name']).lower() != str(categories['id']).lower():
-            categories_add = Categories(name=categories['name'], link_http=categories['url'], id_category=categories['id'])
+            categories_add = Categories(name=categories['name'], link_http=categories['url'],
+                                        id_category=categories['id'])
             connection.connect.add(categories_add)
             count += 1
+            if (count % 10) == 0:
+                print("Récuperation des catégories, ", percentage_calculation(count, total_count), "%",
+                      " d'effectué(s)")
+                clr()
         else:
             count += 1
-        print("Récuperation des catégories, ", percentage_calculation(count, total_count), "%", " d'effectué(s)")
-        cls()
+            if (count % 10) == 0:
+                print("Récuperation des catégories, ", percentage_calculation(count, total_count), "%",
+                      " d'effectué(s)")
+                clr()
+
+    print("Récuperation des catégories 100 % d'effectué(s)")
     connection.connect.commit()
     print("Récupération des catégories réussies")
-    cls()
+    time.sleep(2)
+    clr()
     categories_all = CategoriesQuery.get_categories()
     connection.connect.expunge_all()
 
@@ -71,7 +92,8 @@ if command.lower() == "o":
     final_page = True
 
     while final_page is True:
-        link_page = (lambda url,number_page: str(url) + "/" + str(number_page) + ".json")("https://fr.openfoodfacts.org", number_page)
+        link_page = (lambda url, number_page: str(url) + "/" + str(number_page) + ".json")(
+            "https://fr.openfoodfacts.org", number_page)
         products_dic = requests.get(link_page).json()
         if products_dic['count']:
             total_count = products_dic['count']
@@ -88,13 +110,13 @@ if command.lower() == "o":
                                        nutrition_grade=product['nutrition_grades'], shop=product['stores'],
                                        link_http=product['url'],
                                        categories=CategoriesQuery.get_categories_by_tags(product['categories_tags']))
+                    connection.connect.add(article)
                 except KeyError:
                     continue
 
-                connection.connect.add(article)
             count += 1
-        cls()
-        print("Recuperation des produits, ",percentage_calculation(count,total_count),"%"," d'effectué(s)")
+        clr()
+        print("Recuperation des produits, ", percentage_calculation(count, total_count), "%", " d'effectué(s)")
 
         number_page += 1
     connection.connect.commit()
